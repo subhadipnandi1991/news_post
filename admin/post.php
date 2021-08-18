d290cdc15b7bbafaa4db2676ef9aef2faa81ed4c
subhadipnandi1991 <subha.nandi2014@gmail.com>
2021-08-18 21:29:20 +0530

    Add and show post for particular user role and uploading file done

--- admin/post.php
+++ admin/post.php
@@ -9,6 +9,34 @@
                   <a class="add-new" href="add-post.php">add post</a>
               </div>
               <div class="col-md-12">
+                <?php include 'config.php';
+
+                  if(isset($_GET['page'])) {
+                    $page = $_GET['page'];
+                  } else {
+                    $page = 1;
+                  }
+
+                  $limit = 3;
+                  $offset = ($page - 1) * $limit;
+
+                  if(($_SESSION['user_role']) == '1') {
+                    $sql = "SELECT * FROM post
+                            LEFT JOIN category ON category_id = post.category
+                            LEFT JOIN user on user_id = post.author
+                            ORDER BY post_id DESC LIMIT {$offset}, {$limit}";
+                  } elseif (($_SESSION['user_role']) == '0') {
+                    $sql = "SELECT * FROM post
+                            LEFT JOIN category ON category_id = post.category
+                            LEFT JOIN user on user_id = post.author
+                            WHERE post.author = {$_SESSION['user_id']}
+                            ORDER BY post_id DESC LIMIT {$offset}, {$limit}";
+                  }
+
+
+                  $result = mysqli_query($conn, $sql) or die("Query Failed.");
+                  if (mysqli_num_rows($result) > 0) {
+                ?>
                   <table class="content-table">
                       <thead>
                           <th>S.No.</th>
@@ -20,85 +48,50 @@
                           <th>Delete</th>
                       </thead>
                       <tbody>
+                        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
                           <tr>
-                              <td class='id'>1</td>
-                              <td>Lorem ipsum dolor sit amet</td>
-                              <td>Html</td>
-                              <td>01 Nov, 2019</td>
-                              <td>Admin</td>
+                              <td class='id'><?php echo $row['post_id']; ?></td>
+                              <td><?php echo $row['title']; ?></td>
+                              <td><?php echo $row['category_name']; ?></td>
+                              <td><?php echo $row['post_date']; ?></td>
+                              <td><?php echo $row['first_name']; ?></td>
                               <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
                               <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
-                          <tr>
-                              <td class='id'>2</td>
-                              <td>Lorem ipsum dolor sit amet</td>
-                              <td>Html</td>
-                              <td>01 Nov, 2019</td>
-                              <td>Admin</td>
-                              <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>3</td>
-                              <td>Lorem ipsum dolor sit amet</td>
-                              <td>Html</td>
-                              <td>01 Nov, 2019</td>
-                              <td>Admin</td>
-                              <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>4</td>
-                              <td>Lorem ipsum dolor sit amet</td>
-                              <td>Html</td>
-                              <td>01 Nov, 2019</td>
-                              <td>Admin</td>
-                              <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>5</td>
-                              <td>Lorem ipsum dolor sit amet</td>
-                              <td>Html</td>
-                              <td>01 Nov, 2019</td>
-                              <td>Admin</td>
-                              <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>6</td>
-                              <td>Lorem ipsum dolor sit amet</td>
-                              <td>Html</td>
-                              <td>01 Nov, 2019</td>
-                              <td>Admin</td>
-                              <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>7</td>
-                              <td>Lorem ipsum dolor sit amet</td>
-                              <td>Html</td>
-                              <td>01 Nov, 2019</td>
-                              <td>Admin</td>
-                              <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>8</td>
-                              <td>Lorem ipsum dolor sit amet</td>
-                              <td>Html</td>
-                              <td>01 Nov, 2019</td>
-                              <td>Admin</td>
-                              <td class='edit'><a href='update-post.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
+                        <?php } ?>
                       </tbody>
                   </table>
-                  <ul class='pagination admin-pagination'>
-                      <li class="active"><a>1</a></li>
-                      <li><a>2</a></li>
-                      <li><a>3</a></li>
-                  </ul>
+                <?php }
+                  $sql1 = "SELECT * FROM post";
+                  $result1 = mysqli_query($conn, $sql1) or die("Query Failed!!");
+
+                  if (mysqli_num_rows($result1)) {
+                    $total_rec = mysqli_num_rows($result1);
+
+
+                    $total_pages = ceil($total_rec/ $limit);
+
+                    echo("<ul class='pagination admin-pagination'>");
+                    if ($page > 1) {
+                      echo '<li><a href="post.php?page='.($page - 1).'">Prev</a></li>';
+                    }
+
+                    for ($i = 1; $i <= $total_pages; $i++) {
+
+                      if ($i == $page) {
+                        $active = "active";
+                      } else {
+                        $active = "";
+                      }
+                      echo"<li class=".$active."><a href='post.php?page={$i}'>{$i}</a></li>";
+                    }
+                    if ($total_pages > $page) {
+                      echo '<li><a href="post.php?page='.($page + 1).'">Next</a></li>';
+                    }
+                    echo("</ul>");
+                  }
+                ?>
+
               </div>
           </div>
       </div>
