5775219712393009983c059716a91f4e7a8cc7b7
subhadipnandi1991 <subha.nandi2014@gmail.com>
2021-08-19 16:43:43 +0530

    front page comingnewsare set dynamic from db and detail single page is also set.

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+  $hostname = "http://localhost:4000";
+
+  $conn = mysqli_connect("localhost", "badshah007", "Baadshah@165$", "news-site") or die("Connection Failed:" . mysqli_connect_error());
+?>
