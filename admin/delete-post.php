0446a0caf960331c5f78115941cdcb599bf6eb13
subhadipnandi1991 <subha.nandi2014@gmail.com>
2021-08-19 15:47:25 +0530

    Delete images from folder when delete a record. And prevent access any sub directory through web browser.

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -4,6 +4,16 @@
   $post_id = $_GET['id'];
   $cat_id = $_GET['catid'];
 
+  $sql1 = "SELECT * FROM post WHERE post_id={$post_id}";
+  $result1 = mysqli_query($conn, $sql1) or die("Query failed: Select.");
+  $row = mysqli_fetch_assoc($result1);
+  // echo "<pre>";
+  // print_r($row);
+  // echo "</pre>";
+  // die();
+
+  unlink("upload/".$row['post_img']);
+  
   $sql = "DELETE FROM post WHERE post_id={$post_id};";
   $sql .= "UPDATE category SET post = post-1 WHERE category_id={$cat_id}";
 
---
+++ css/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>File not found</title>
+  </head>
+  <body>
+    <h1>File not found</h1>
+  </body>
+</html>
